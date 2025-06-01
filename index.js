eb15cac82e2bba4d6ae569b0e821ad0d3664fe1c
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-02 00:13:31 +0530

    Add server routes for home and about pages, handle 404 and method errors, and update package scripts for nodemon

--- index.js
+++ index.js
@@ -2,11 +2,34 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-    res.write("Hello World!!!");
-    res.end("Hello World!!!");
+
+    try {
+        
+        if(req.method == 'GET') {
+
+            if(req.url == '/') {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end("Home Page");
+            } else if (req.url == '/about') {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end("About Page");
+            } else {
+                res.writeHead(404, { 'Content-Type' : 'text/plain' });
+                res.end("404 Not Found");
+            }
+        } else {
+            throw new Error("Method Not Allowed")
+        }
+        
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(`Internal Server Error: ${error.message}`);
+        
+    }
+
 })
 
-const port = 3000;
+const port = process.env.PORT;
 server.listen(port , () => {
     console.log(`Server is running on port ${port}`);
 })
